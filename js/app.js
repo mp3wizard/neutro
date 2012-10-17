8aea6cb0341e59120539e8de9e12bb64e8fa8007
Sappawish Siripon <mp3wizard@gmail.com>
2012-10-17 09:52:19 +0700

    Update Landing Page for "FAQ"

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
 jQuery(document).ready(function($){ 
 	$('a[rel=tooltip]').tooltip();
-	$(".royalSlider").royalSlider({
-        loop: true,
-        autoHideArrows: true,
-        arrowsNavHideOnTouch: true,
-        keyboardNavEnabled: true,
-        imageScalePadding: 0,
-        autoPlay: {
-    		// autoplay options go gere
-    		enabled: true,
-    		pauseOnHover: true,
-    		delay: 5000
-    	}
-    });  
+    $('.pagination a.active').click(function() { 
+	    return false;
+    })
 });
