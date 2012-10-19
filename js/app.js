02857d2ee8f2717f750846ef7d8e90468fb92452
Sappawish Siripon <mp3wizard@gmail.com>
2012-10-19 14:30:02 +0700

    Update link to go to FAQ page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function($){ 
 	$('a[rel=tooltip]').tooltip();
+	$('a[rel=tooltip]').click(function() { 
+		return false;
+	})
     $('.pagination a.active').click(function() { 
 	    return false;
     })
